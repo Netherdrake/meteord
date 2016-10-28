203659ddce9a3e4383f02209c3628a49d81533d1
Netherdrake <Netherdrake@users.noreply.github.com>
2016-10-28 03:31:39 +0200

    workaround "root" bug

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,3 +1,4 @@
 set -e
 
-curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
+export METEOR_NO_RELEASE_CHECK=true
+curl https://install.meteor.com/?release=1.4.1.3 | sed s/--progress-bar/-sL/g | /bin/sh
